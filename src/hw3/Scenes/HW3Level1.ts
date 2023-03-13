d66d47aea6430b498cad032ec33212d9aca200ee
jcJoeyChan <joeychan1012@gmail.com>
2023-03-13 15:45:56 -0400

    PART 5

--- src/hw3/Scenes/HW3Level1.ts
+++ src/hw3/Scenes/HW3Level1.ts
@@ -80,8 +80,11 @@ export default class Level1 extends HW3Level {
      */
     public unloadScene(): void {
         // TODO decide which resources to keep/cull 
-        
-
+        this.resourceManager.keepSpritesheet(this.playerSpriteKey);
+        this.resourceManager.keepAudio(this.jumpAudioKey);
+        this.resourceManager.keepAudio(this.deathAudioKey);
+        this.resourceManager.keepAudio(this.tileDestroyedAudioKey);
+        this.resourceManager.unloadAllResources();
     }
     
     public startScene(): void {
